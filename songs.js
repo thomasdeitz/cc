e4a2b3025995261f15ed94b14ac948fc9aa11dd8
Thomas Deitz <thomasdeitz@gmail.com>
2022-05-12 09:01:15 -0500

    move shows and songs data to individual files.

---
+++ songs.js
@@ -0,0 +1,102 @@
+const songs = [
+    {
+        name: 'Shooting Stars',
+        artist: 'Rival Sons'
+      },
+      {
+        name: 'When Nature Calls',
+        artist: 'Original'
+      },
+      {
+        name: 'House of the Rising Sun',
+        artist: 'The Animals'
+      },
+      {
+        name: 'Doesn\'t Matter',
+        artist: 'Original'
+      },
+      {
+        name: 'Come Together',
+        artist: 'The Beatles'
+      },
+      {
+        name: 'Imaginary Nightmare',
+        artist: 'Original'
+      },
+      {
+        name: 'Can’t You See',
+        artist: 'The Marshall Tucker Band'
+      },
+      {
+        name: 'Sister Bay',
+        artist: 'Original'
+      },
+      {
+        name: 'Hurt',
+        artist: 'Johnny Cash'
+      },
+      {
+        name: 'Not The Same',
+        artist: 'Original'
+      },
+      {
+        name: 'Waiting for the Thunder',
+        artist: 'Blackberry Smoke'
+      },
+      {
+        name: 'Fraser',
+        artist: 'Original'
+      },
+      {
+        name: 'Have You Ever Seen The Rain?',
+        artist: 'Creedence Clearwater Revival'
+      },
+      {
+        name: 'Listen To You',
+        artist: 'Original'
+      },
+      {
+        name: 'Tennessee Whiskey',
+        artist: 'Chris Stapleton'
+      },
+      {
+        name: 'Midnight Rider',
+        artist: 'The Allman Brothers'
+      },
+      {
+        name: 'Seven Nation Army',
+        artist: 'The White Stripes'
+      },
+      {
+        name: '(I Can\'t Get No) Satisfaction',
+        artist: 'The Rolling Stones'
+      },
+      {
+        name: 'The Well',
+        artist: 'Marcus King'
+      },
+      {
+        name: 'Revolution Song',
+        artist: 'Original'
+      },
+      {
+        name: 'The Stone',
+        artist: 'Original'
+      },
+      {
+        name: 'One Horse Town',
+        artist: 'Blackberry Smoke'
+      },
+      {
+        name: 'Midnight Train to Memphis',
+        artist: 'The  Steal Drivers'
+      },
+      {
+        name: 'TNT',
+        artist: 'AC/DC'
+      },
+      {
+        name: 'The Devil Wears a Suit and Tie',
+        artist: 'Colter Wall'
+      }
+];
