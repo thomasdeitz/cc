a2afce74c9e5ebe6cd8b72abbbe05cc73f1abb8d
Thomas Deitz <info@thomasdeitz.com>
2023-02-21 11:04:40 -0600

    update video

--- components/videos.vue.js
+++ components/videos.vue.js
@@ -3,7 +3,7 @@ const Videos = {
     <div class="videos">
         <iframe src="https://www.youtube.com/embed/fSp1kbXYaNA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe src="https://www.youtube.com/embed/Ob8DSe4vCNo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe src="https://www.youtube.com/embed/EyWVCOmb5xI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/SoEMNNSE3bQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   `
 }
