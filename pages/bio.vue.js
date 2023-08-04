5ae33420cf8da0a380ffe2e813c733b3d37aa853
thomasdeitz <thomasjdeitz@gmail.com>
2023-08-04 08:31:40 -0500

    bio update

--- pages/bio.vue.js
+++ pages/bio.vue.js
@@ -1,12 +1,12 @@
 const BioPage = {
     template: `
     <Section heading="About the Band">
-    <p>
-        Since its inception in 2019 by then 10 year old identical twins Isaac and Lucas Deitz, Candy Cigarette has been bringing a unique blend of blues and strait-up rock n' roll to audiences across their home state of Wisconsin and beyond. Playing shows at the Turner Hall Ballroom, Blues from the Top Music Festival, Summerfest, The Miramar Theatre The Galaxie Theatre and the Homegrown Blues Festival supporting artists like Jared James Nichols, Bourbon House, The Record Company, and Larkin Poe. 
-    </p>
-    <p>
-        Featuring Lucas - vocals/guitar, Isaac - vocals/bass and Evan - Drums, this trio is set to help usher in the next generation of powerful blues rock.
-    </p>
+        <p>
+            Since its inception in 2019 by then 10 year old identical twins Isaac and Lucas Deitz, Candy Cigarette has been bringing a unique blend of blues and strait-up rock n' roll to audiences across their home state of Wisconsin and beyond. Playing shows at the Turner Hall Ballroom, Blues from the Top Music Festival, Summerfest, The Miramar Theatre and the Homegrown Blues Festival supporting artists like Jared James Nichols, Bourbon House and The Jimmy's. 
+        </p>
+        <p>
+            Featuring Lucas - vocals/guitar, Isaac - vocals/bass and Evan - Drums, this trio is set to help usher in the next generation of powerful blues rock.
+        </p>
     </Section>
     `
 }
