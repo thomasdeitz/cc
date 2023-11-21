1cb4feb9a340ad4ceee63da1a16b25af3adf732a
thomasdeitz <thomasjdeitz@gmail.com>
2023-11-21 15:14:45 -0600

    bio update

--- pages/bio.vue.js
+++ pages/bio.vue.js
@@ -2,7 +2,7 @@ const BioPage = {
     template: `
     <Section heading="About the Band">
         <p>
-            Since its inception in 2019 by then 10 year old identical twins Isaac and Lucas Deitz, Candy Cigarette has been bringing a unique blend of blues and straight-up rock n' roll to audiences across their home state of Wisconsin and beyond. Playing shows at the Turner Hall Ballroom, Blues from the Top Music Festival, Summerfest, The Miramar Theatre and Buddy Guy's Legends supporting artists like Jared James Nichols, Bourbon House and The Jimmy's. In addition the group has recently been selected to represent the Paramount Music Association in the 2024 International Blues Challenge - Youth Showcase.
+            Since its inception in 2019 by then 10 year old identical twins Isaac and Lucas Deitz, Candy Cigarette has been bringing a unique blend of blues and straight-up rock n' roll to audiences across their home state of Wisconsin and beyond - playing shows at the the Genesse Theatre, Turner Hall Ballroom, the High Noon Saloon, Blues from the Top Music Festival, Summerfest, The Miramar Theatre and Buddy Guy's Legends supporting artists like Blackberry Smoke, Jared James Nichols, Cage Willis, Bourbon House and The Jimmy's. In addition the group has recently been selected to represent the Paramount Music Association in the 2024 International Blues Challenge - Youth Showcase.
         </p>
         <p>
             Featuring Lucas - vocals/guitar, Isaac - vocals/bass/guitar and Evan - drums, this trio is set to help usher in the next generation of powerful blues rock.
