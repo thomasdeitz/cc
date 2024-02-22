548ce4eb73595e108a8284ad5eca5f83dd475fdf
thomasdeitz <thomasjdeitz@gmail.com>
2024-02-21 22:22:39 -0600

    photo on about

--- components/section.vue.js
+++ components/section.vue.js
@@ -2,9 +2,10 @@ const Section = {
     props: [
         'heading',
         'background',
+        'layout'
     ],
     template: `
-        <section :class="{ primary: background === 'primary', black: background === 'black' }">
+        <section :class="{ split: layout === 'split', primary: background === 'primary', black: background === 'black' }">
             <div class="container">
                 <h2>{{heading}}</h2>
                 <slot></slot>
