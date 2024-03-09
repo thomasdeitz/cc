dc1d608e6deee333f47402dce5c8161f02f4ac02
thomasdeitz <thomasjdeitz@gmail.com>
2024-03-09 11:56:13 -0600

    move scripts

--- components/subscribe.vue.js
+++ components/subscribe.vue.js
@@ -1,7 +1,6 @@
 const Subscribe = {
     template: `
         <div id="mc_embed_shell">
-            <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
             <div id="mc_embed_signup">
                 <form action="https://candycigaretteband.us21.list-manage.com/subscribe/post?u=c6afa7a3a3f37ac9a34c6c3c3&amp;id=b6ea119396&amp;f_id=0063f7e6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
                     <div id="mc_embed_signup_scroll">
@@ -27,7 +26,6 @@ const Subscribe = {
                     </div>
                 </form>
             </div>
-            <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
         </div>
     `
 }
