cae25f158ae71fa4127b293e06aa62e4ccd9a19c
thomasdeitz <thomasjdeitz@gmail.com>
2025-02-08 17:45:56 -0600

    update no tribe

--- data/shows.js
+++ data/shows.js
@@ -1571,12 +1571,12 @@ const shows = [
     link: "https://aftontickets.com/event/buyticket/8qj4l2v79m",
     notes: "Supporting Stay Outside w/ Modern Haunting"
   },
-  {
-    datetime: "2025-03-08T10:00",
-    venue: "Tribe Bar & Grill",
-    location: "Cudahy, WI",
-    notes: "w/ Broken Arrow & The Coves",
-  },
+  // {
+  //   datetime: "2025-03-08T10:00",
+  //   venue: "Tribe Bar & Grill",
+  //   location: "Cudahy, WI",
+  //   notes: "w/ Broken Arrow & The Coves",
+  // },
   {
     datetime: "2025-03-15T18:00",
     venue: "Heroes Venture Arcade",
