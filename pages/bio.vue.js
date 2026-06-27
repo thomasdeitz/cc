68f105d061be79511478bd9a139291901b737a89
thomasdeitz <thomasjdeitz@gmail.com>
2026-06-27 15:19:07 -0600

    align

--- pages/bio.vue.js
+++ pages/bio.vue.js
@@ -2,7 +2,7 @@ const BioPage = {
     template: `
     <Hero />
     <Section alignment="center" heading="About the Band">
-        <div class="two-column">
+        <div class="">
 Out of Oconomowoc, Wisconsin, Candy Cigarette delivers a blistering brand of raw, high-energy blues-rock fueled by an unapologetic spirit. Identical twins Isaac (bass/vocals) and Lucas Deitz (guitar/vocals), alongside drummer Evan Sterken, are igniting stages across the Midwest and beyond.
 <br/><br/>
 Their sound blends the massive swagger of vintage-era rock with the raw fire of heavy blues, captivating audiences through electrifying live performances, searing guitar riffs, blood harmonies, and a thunderous rhythmic drive. Built for the stage, Candy Cigarette commands any crowd. Their latest single, "REVOLUTION," marks a powerful step forward, with more new music on the horizon.
--- pages/presskit.vue.js
+++ pages/presskit.vue.js
@@ -3,7 +3,7 @@ const PressKit = {
     <Hero />
     
     <Section background="" heading="Biography" alignment="center">
-        <div class="two-column">
+        <div class="">
 Out of Oconomowoc, Wisconsin, Candy Cigarette delivers a blistering brand of raw, high-energy blues-rock fueled by an unapologetic spirit. Identical twins Isaac (bass/vocals) and Lucas Deitz (guitar/vocals), alongside drummer Evan Sterken, are igniting stages across the Midwest and beyond.
 <br/><br/>
 Their sound blends the massive swagger of vintage-era rock with the raw fire of heavy blues, captivating audiences through electrifying live performances, searing guitar riffs, blood harmonies, and a thunderous rhythmic drive. Built for the stage, Candy Cigarette commands any crowd. Their latest single, "REVOLUTION," marks a powerful step forward, with more new music on the horizon.
